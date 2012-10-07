e7f65af17f8c76bacf4b22b9643ef9bf52209634
Wonko7 <Wonko@undefined.re>
2012-10-07 13:32:42 +0200

    misc dot files

---
+++ amixer.sh
@@ -0,0 +1,6 @@
+amixer -q set "Surround Speaker Playback Volum" 100% unmute
+amixer -q set "Surround Speaker" 100% unmute
+amixer -q set "Front Speaker" 100% unmute
+amixer -q set "Master" 40% unmute
+amixer -q set "Headphone" 95% unmute
+amixer -q set "PCM" 100% unmute
